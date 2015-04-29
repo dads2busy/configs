5c9d25907fe4cab55eb916977f67c79f2a144f91
Aaron Schroeder <aschroed@vbi.vt.edu>
2015-04-29 15:02:38 -0400

    fuses

---
+++ fuses.sh
@@ -0,0 +1,18 @@
+OLDER=home_on_snowmane
+LINK=sdal
+if [[ $(df | grep $FOLDER | wc -l) -eq 0 ]]; then
+  mkdir -p ~/home_on_snowmane
+  sshfs $USER@snowmane.vbi.vt.edu:/home/$USER ~/$FOLDER -o 
+reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,fo$
+  echo "$FOLDER mounted"
+else
+  echo "$FOLDER mounted"
+fi
+
+symlink=~/$LINK
+if ! [ -L "$symlink" ]; then
+  ln -s ~/$FOLDER/$LINK ~/$LINK
+  echo "Link $LINK created"
+fi
+
+
